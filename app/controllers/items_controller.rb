e257ad88832321af42f1e4acd1526960041db0b6
kdeluz <kdeluz@rrc.ca>
2024-06-26 17:57:16 -0500

    added sort, adjusted based on alphabetical, gold asc and gold desc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
     def index
       @tags = Tag.all
+      @sort = params[:sort]
+  
       if params[:tag]
         logger.debug "Filtering items by tag: #{params[:tag]}"
         tag = Tag.find_by(name: params[:tag])
@@ -14,10 +16,27 @@ class ItemsController < ApplicationController
       else
         @items = Item.includes(:gold, :stat, :tags).all
       end
+  
+      @items = sort_items(@items, @sort)
     end
   
     def show
       @item = Item.find(params[:id])
+    end
+  
+    private
+  
+    def sort_items(items, sort)
+      case sort
+      when 'alphabetical'
+        items.order(:name)
+      when 'gold_ascending'
+        items.joins(:gold).order('golds.total ASC')
+      when 'gold_descending'
+        items.joins(:gold).order('golds.total DESC')
+      else
+        items
+      end
     end
   end
   
